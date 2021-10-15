a8b98d567269e960e4a7255f57777dac6192ea88
jht301 <jackhterzi@gmail.com>
2021-10-15 13:59:28 -0400

    Simple Math
    Includes Variables, Comments, and multi-line statements

---
+++ Arithmatic/Simple math 1.py
@@ -0,0 +1,17 @@
+a = 1+1
+print(a)
+b = 3*5
+c = a + b
+# Comments begin with the pound sign
+# on the next like I use the backslash to continue the arithmatic to the next line
+a = c - b \
+    + 3
+
+#Here I use parentheses. I can Also use {} or [] to do the same thing
+'''
+I could also have commented
+Multiple lines like this
+'''
+a = (a + a -
+     -a)
+print(a)
